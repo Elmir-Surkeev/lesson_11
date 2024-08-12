52dd28e068f13f29410fa3ef81b644f3e61bf5d4
Elmirbek Surkeev <elmirsurkeev@gmail.com>
2024-08-13 03:49:42 +0600

    added logic about add random products int array

--- src/Product.java
+++ src/Product.java
@@ -1,8 +1,11 @@
 import java.time.LocalDate;
 import java.util.Random;
 public abstract class Product {
+    Random rnd = new Random();
     private String name;
     private int bestBeforeDate;
+    private boolean inIcebox;
+
     public Product(String name,int bestBeforeDate){
         this.name = name;
         this.bestBeforeDate = bestBeforeDate;
@@ -13,8 +16,11 @@ public abstract class Product {
     void isFresh(){
         Random rnd = new Random();
         int random = rnd.nextInt(1, 200);
+
         LocalDate produceOn = LocalDate.now().minusDays(random); // Дата производства
         LocalDate expiryDate = produceOn.plusDays(bestBeforeDate); // Дата истечения срока годности
+
+      //  if (placeProduct())
         if (LocalDate.now().isBefore(expiryDate) || bestBeforeDate == 0) {;
             System.out.println("Продукт свежий, произведен: " + produceOn );
             System.out.println("Можно употреблять до: " + expiryDate );
@@ -29,5 +35,12 @@ public abstract class Product {
 
         }
     }
+//    void placeProduct(){
+//        int place = rnd.nextInt(1);
+//        if (place == 0){
+//            inIcebox = true;
+//        }else if (place==1){
+//            inIcebox = false;
+//        }
 
 }
--- src/Store.java
+++ src/Store.java
@@ -1,3 +1,42 @@
+import java.util.Random;
 public class Store {
+    public static void main(String[] args) {
+        Product[] products = new Product[20];
+        Random rnd = new Random();
 
+
+        for(int i = 0; i< products.length; i++){
+
+            int productType = rnd.nextInt(4);
+            switch (productType){
+                case 0:
+                    products[i] = new Milk("Milk", 60);
+                    break;
+                case 1:
+                    products[i] = new Salt("Salt", 0);
+                    break;
+                case 2:
+                    products[i] = new Fish("Fish", 20);
+                    break;
+                case 3:
+                    products[i] = new Corn("Corn", 150);
+                    break;
+                case 4:
+                    products[i] = new Stew("Stew", 180);
+                    break;
+                default:
+                    System.out.println("Error");
+                    break;
+            }
+        }
+
+    polym(products);
+    }
+
+
+    public static void polym(Product[] products) {
+        for (Product product : products) {
+            product.isFresh();
+        }
+    }
 }
